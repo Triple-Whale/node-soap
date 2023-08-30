48b205502912405078f6a34fc86413ee18e6310d
cheskel <twerskycheskel@gmail.com>
2023-08-30 09:59:13 +0300

    update axios

--- src/client.ts
+++ src/client.ts
@@ -511,6 +511,7 @@ export class Client extends EventEmitter {
           getStream(res.data).then((body) => {
             this.lastResponse = body;
             this.lastElapsedTime = Date.now() - startTime;
+            // @ts-ignore
             this.lastResponseHeaders = res && res.headers;
             // Added mostly for testability, but possibly useful for debugging
             this.lastRequestHeaders = res.config && res.config.headers || res.headers;
@@ -529,6 +530,7 @@ export class Client extends EventEmitter {
           this.wsdl.xmlToObject(res.data, (error, obj) => {
             this.lastResponse = res;
             this.lastElapsedTime = Date.now() - startTime;
+            // @ts-ignore
             this.lastResponseHeaders = res && res.headers;
             // Added mostly for testability, but possibly useful for debugging
             this.lastRequestHeaders = res.config.headers;
